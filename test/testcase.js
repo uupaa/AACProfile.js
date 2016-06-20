775efe92e7a763d37d80a8cd1d9c53caaedbb0fc
uupaa <uupaa.js@gmail.com>
2016-06-20 18:27:19 +0900

    support HE-AAC v2

--- test/testcase.js
+++ test/testcase.js
@@ -29,8 +29,9 @@ function testAACProfile(test, pass, miss) {
     var result = {
             1: _getProfile("mp4a.40.2, avc1.66.13")  === "AAC-LC",
             2: _getProfile("mp4a.40.5, avc1.42c00d") === "HE-AAC",
-            3: _getProfile("mp4a.40.34, avc1.66.30") === "MP3",
-            4: _getProfile("mp4a.40.99, avc1.66.30") === "", // UNKNOWN Profile
+            3: _getProfile("mp4a.40.29, avc1.42c00d") === "HE-AAC v2",
+            4: _getProfile("mp4a.40.34, avc1.66.30") === "MP3",
+            5: _getProfile("mp4a.40.99, avc1.66.30") === "", // UNKNOWN Profile
         };
 
     if ( /false/.test(JSON.stringify(result)) ) {
